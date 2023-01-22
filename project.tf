1926abc2335a8cebec38bbba110a63e17c0bf0c8
aokuyama <ao980s@gmail.com>
2023-01-22 10:07:45 +0900

    update cmd

--- project.tf
+++ project.tf
@@ -1,8 +1,12 @@
 resource "vercel_project" "app" {
-  name      = "aokuyama-portfolio"
-  framework = "nextjs"
+  name             = "aokuyama-portfolio"
+  framework        = "nextjs"
+  build_command    = "yarn workspace web build"
+  install_command  = "yarn workspace web install"
+  output_directory = "apps/web/.next"
   git_repository = {
-    type = "github"
-    repo = "aokuyama/portfolio"
+    type              = "github"
+    repo              = "aokuyama/portfolio"
+    production_branch = "deploy"
   }
 }
